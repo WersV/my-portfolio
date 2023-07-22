2ad77328f3e83905cf85284826292f561998075b
Przemysław Botwina <przemek.botwina@o2.pl>
2023-07-22 22:41:41 +0200

    add .wrapper styles mobile

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,45 +1,41 @@
 <template>
   <main class="about-view">
-    <h1>About me</h1>
+    <h1>About <span>me</span></h1>
     <div class="wrapper">
       <section class="personal-info">
       <h2>Personal Info</h2>
-      <img src="@/assets/img/portfolio-photo-mobile.jpg" alt="portfolio owner" v-if="isLowRes">
+      <img src="@/assets/img/portfolio-photo-mobile.jpg" alt="portfolio owner">
       <section class="details">
-        <div>
-          <p>first name:</p>
+        <div class="detail">
+          <p>First Name:</p>
           <span>Przemysław</span>
         </div>
-        <div>
+        <div class="detail">
           <p>Last Name:</p>
           <span>Botwina</span>
         </div>
-        <div>
-          <p>Age:</p>
-          <span>24 Years</span>
-        </div>
-        <div>
-          <p>Phone:</p>
-          <span>736 245 290</span>
-        </div>
-        <div>
-          <p>Email:</p>
-          <span>botwina.przemyslaw@gmail.com</span>
-        </div> 
-        <div>
+        <div class="detail detail-link">
           <p>Git:</p>
           <a href="https://github.com/WersV">See my profile</a>
         </div>
-        <div>
+        <div class="detail detail-link">
           <p>Codewars:</p>
-          <a href="https://www.codewars.com/users/WersV/completed">See completed challenges</a>
+          <a href="https://www.codewars.com/users/WersV/completed">See challenges</a>
         </div>
-        <div>
+        <div class="detail">
           <p>Languages:</p>
           <span>Polish, English(B2)</span>
         </div>
+        <div class="detail">
+          <p>Phone:</p>
+          <span>736 245 290</span>
+        </div>
+        <div class="detail">
+          <p>Email:</p>
+          <span>botwina.przemyslaw@gmail.com</span>
+        </div> 
       </section>
-      <button>Download CV</button>
+      <button>Download CV <span><font-awesome-icon icon="fa-solid fa-arrow-right" /></span></button>
     </section>
     <section class="key-features">
       <div>
@@ -111,6 +107,56 @@ export default {
   
 }
 </script>
-<style>
-  
+<style lang="scss">
+@import '@/assets/_variables.scss';
+  .about-view {
+    margin: 0 20px;
+    h1 {
+      color: #666666;
+      text-transform: uppercase;
+      font-weight: 900;
+      margin: 50px 0;
+      span {
+        color: #FFB400;
+      }
+    }
+    .wrapper {
+      .personal-info {
+        h2 {
+          color:#666666;
+          text-transform: uppercase;
+          margin: 30px 0;
+        }
+        img {
+          @include img-styles;
+        }
+      }
+      .details {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 20px 0;
+        gap: 18px;
+        .detail {
+          flex-basis: 46%;
+          line-height: 24px;
+          p {
+            color: rgb(139, 139, 139);
+          }
+          span {
+            color: #666666;
+            font-weight: 700;
+          }
+        }
+        .detail-link {
+          a {
+            text-decoration: none;
+            color: #666666;
+            font-weight: 700;
+          }
+        }
+      }
+      @include btn-styles-anim(250px);
+    }
+  }
+  // zrób propsy isLowRes z App.vue do child components
 </style>
