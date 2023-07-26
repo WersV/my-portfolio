7191ae33f81f2b6e5aa0d86fce8da9531505fd5e
Przemysław Botwina <przemek.botwina@o2.pl>
2023-07-26 23:58:52 +0200

    add screen size fun on resize event in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,24 @@
 import LightSwitcher from '@/components/AppLightSwitcher.vue'
 import NavMenu from '@/components/AppNavMenu.vue'
 import {RouterView} from 'vue-router'
+import {onMounted} from 'vue';
+import {useGlobalVariables} from '@/stores/globalVariables'
+
+
+const globalVariables = useGlobalVariables();
+onMounted(() => {
+  window.addEventListener('resize', globalVariables.toggleIsLowRes);
+  globalVariables.toggleIsLowRes();
+})
 </script>
 
 <template>
+  <div class="animation-wrapper"></div>
   <LightSwitcher/>
   <NavMenu/>
-  <RouterView />
+  <Transition>
+    <RouterView />
+  </Transition>
 </template>
 
 <style lang="scss">
@@ -15,8 +27,27 @@ import {RouterView} from 'vue-router'
 @import './assets//global.css';
 body {
   background-color: $bcg-color-light;
+  // .animation-wrapper {
+  //   position: fixed;
+  //   top: 0;
+  //   transform: translateY(0%);
+  //   width: 100%;
+  //   height: 100vh;
+  //   background-color: #272727;
+  //   z-index: 3;
+  //   transition: margin-top 1s;
+  // }
 }
 
+.v-enter-active,
+.v-leave-active {
+  transition: all .3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateY(100%);
+}
 //dark mode 
 .dark body {
   background-color: $bcg-color-dark;
