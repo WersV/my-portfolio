5ed038bd6978fd8f8596fc95e5bececce57dfd3d
Przemysław Botwina <przemek.botwina@o2.pl>
2023-07-28 21:08:52 +0200

    add imgs and h1

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,11 +1,48 @@
 <template>
-  <h1>PortfolioView</h1>
+  <main class="portfolio-view">
+    <h1>My <span>portfolio</span></h1>
+    <div class="projects-wrapper">
+      <section class="project">
+        <img src="@/assets/img/psychology-specialist-screen.png" alt="">
+      </section>
+      <section class="project">
+        <img src="@/assets/img/ip-tracker-screen.png" alt="">
+      </section>
+      <section class="project">
+        <img src="@/assets/img/recipes-screen.png" alt="">
+      </section>
+    </div>
+  </main>
 </template>
 <script>
 export default {
   
 }
 </script>
-<style>
-  
+<style lang="scss">
+@import '@/assets/_variables.scss';
+  .portfolio-view {
+    h1 {
+      color: $text-light-gray;
+      font-size: 35px;
+      text-transform: uppercase;
+      font-weight: 900;
+      span {
+        display: block;
+        color: $text-yellow;
+      }
+    }
+    .projects-wrapper {
+      width: 100%;
+      .project {
+        width: 300px;
+        height: 300px;
+        img {
+          display: block;
+          max-width:90%;
+          max-height:90%;
+        }
+      }
+    }
+  }
 </style>
