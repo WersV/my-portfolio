1d8c2c2eb09e01e62fae601d124258cfdfa249d7
Przemysław Botwina <przemek.botwina@o2.pl>
2023-07-28 21:07:14 +0200

    little fixes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -117,7 +117,7 @@ export default {
       font-weight: 900;
       margin: 50px 0;
       span {
-        color: #FFB400;
+        color: $text-yellow;
       }
     }
     .wrapper {
@@ -150,13 +150,95 @@ export default {
         .detail-link {
           a {
             text-decoration: none;
+<<<<<<< Updated upstream
             color: #666666;
+=======
+            color: $text-yellow;
+>>>>>>> Stashed changes
             font-weight: 700;
           }
         }
       }
+<<<<<<< Updated upstream
       @include btn-styles-anim(250px);
     }
+=======
+      .key-features {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      // gap: 10px;
+      .feature {
+        flex-basis: 40%;
+        // border: 1px solid #cecece;
+        box-shadow: 0 8px 6px -6px #cecece;
+        padding: 20px;
+        span {
+          color: $text-yellow;
+          font-size: 32px;
+          font-weight: 700;
+        }
+        p {
+          color: $text-light-gray;
+          width: 80px;
+          text-transform: uppercase;
+        }
+      }
+      .feature:last-child {
+        span {
+          font-size: 35px;
+          line-height: 30px;
+        }
+      }
+     }
+      @include btn-styles-anim(250px);
+    }
+    .separator {
+      width: 50%;
+      max-width: 1280px;
+      border-bottom: 1px solid #d8d8d8;
+      margin: 60px auto 22px auto;
+    }
+    .my-skills {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      h2 {
+        @include about-me-h2;
+      }
+      h3 {
+        font-size: 16px;
+        color: rgb(139, 139, 139);
+        text-transform: uppercase;
+        margin: 20px 0;
+      }
+      .skills-wrapper {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 10px;
+        .skill {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
+          width: 120px;
+          height: 120px;
+          border: 2px solid $text-yellow;
+          border-radius: 50%;
+          text-transform: uppercase;
+          span {
+            color: $text-light-gray;
+            font-weight: 700;
+          }
+        }
+      }
+    }
+    .my-skills .skills-wrapper:last-child {
+      margin-bottom: 80px;
+    }
+>>>>>>> Stashed changes
   }
   // zrób propsy isLowRes z App.vue do child components
 </style>
